1b821643bdf6f47fd13cedb9d6f681d17d64e4d3
Vitaliy Yakovlev <vintyak@gmail.com>
2025-09-02 23:23:08 +0300

    Refactor ProxyStarter using ExecutorService, edit About

--- src/main/java/com/yakovitalik/fixdpi/ProxyStarter.java
+++ src/main/java/com/yakovitalik/fixdpi/ProxyStarter.java
@@ -1,12 +1,18 @@
 package com.yakovitalik.fixdpi;
 
-public class ProxyStarter {
-    public static void startProxy() {
-        Thread newThread = new Thread(() -> {
-            var proxyServer = new ProxyServer();
-            proxyServer.startProxy();
-        });
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-        newThread.start();
+public class ProxyStarter implements Runnable {
+    public static void startProxy() {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(new ProxyStarter());
+        executorService.shutdown();
+    }
+
+    @Override
+    public void run() {
+        var proxyServer = new ProxyServer();
+        proxyServer.startProxy();
     }
 }
